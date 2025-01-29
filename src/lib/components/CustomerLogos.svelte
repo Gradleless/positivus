b0551cedea1bc07265ac6d678fd32f06d5169426
jeoooo <jeocarlolubao@gmail.com>
2025-01-29 12:32:18 +0800

    feat: add Hero and CustomerLogos components

---
+++ src/lib/components/CustomerLogos.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Hero from './Hero.svelte';
+</script>
+
+<div class="flex flex-wrap justify-center gap-8 py-10 md:gap-x-32">
+	<img src="/Company logo-6.png" alt="" class="h-12 w-auto grayscale filter" />
+	<img src="/Company logo-5.png" alt="" class="h-12 w-auto grayscale filter" />
+	<img src="/Company logo-1.png" alt="" class="h-12 w-auto grayscale filter" />
+	<img src="/Company logo-2.png" alt="" class="h-12 w-auto grayscale filter" />
+	<img src="/Company logo-3.png" alt="" class="h-12 w-auto grayscale filter" />
+	<img src="/Company logo-4.png" alt="" class="h-12 w-auto grayscale filter" />
+</div>
+
+<style>
+	.space-grotesk-300 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 300;
+		font-style: normal;
+	}
+
+	.space-grotesk-400 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	.space-grotesk-500 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 500;
+		font-style: normal;
+	}
+
+	.space-grotesk-600 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 600;
+		font-style: normal;
+	}
+
+	.space-grotesk-700 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 700;
+		font-style: normal;
+	}
+</style>
---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,57 @@
+<div
+	class=" space-grotesk-400 flex flex-col-reverse items-center justify-between bg-white px-6 pt-10 md:flex-row md:px-28"
+>
+	<div class="flex w-full flex-col items-center text-center md:w-1/3 md:items-start md:text-left">
+		<h1 class="mb-4 text-4xl font-bold md:mb-16 md:text-6xl">
+			Navigating the digital landscape for success
+		</h1>
+		<h3 class="text-sm md:text-base">
+			Our digital marketing agency helps businesses grow and succeed online through a range of
+			services including SEO, PPC, social media marketing, and content creation.
+		</h3>
+		<button class="mt-4 rounded bg-[#191A23] px-4 py-2 text-white md:px-6 md:py-3">
+			Book a Consultation
+		</button>
+	</div>
+	<div class="mb-8 flex w-full items-center justify-center md:mb-0 md:w-auto">
+		<!-- svelte-ignore a11y_img_redundant_alt -->
+		<img src="/Illustration.png" alt="Hero Image" class="h-1/5 w-auto md:h-1/2" />
+	</div>
+</div>
+
+<style>
+	.space-grotesk-300 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 300;
+		font-style: normal;
+	}
+
+	.space-grotesk-400 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	.space-grotesk-500 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 500;
+		font-style: normal;
+	}
+
+	.space-grotesk-600 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 600;
+		font-style: normal;
+	}
+
+	.space-grotesk-700 {
+		font-family: 'Space Grotesk', serif;
+		font-optical-sizing: auto;
+		font-weight: 700;
+		font-style: normal;
+	}
+</style>
