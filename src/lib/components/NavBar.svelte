0fb629ed75800293db9a89a8aca68874ed763d48
jeoooo <jeocarlolubao@gmail.com>
2025-01-29 12:32:11 +0800

    feat: add footer and navbar components

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,41 @@
+<div
+	class="h-17 space-grotesk-500 flex items-center justify-between bg-white px-4 py-4 shadow-md md:px-28"
+>
+	<div class="text-2xl font-bold">
+		<img src="/Logo.png" alt="Logo" />
+	</div>
+	<div class="hidden items-center gap-4 md:flex md:gap-16">
+		<a href="#about-us" class="text-gray-800">About Us</a>
+		<a href="#services" class="text-gray-800">Services</a>
+		<a href="#use-cases" class="text-gray-800">Use Cases</a>
+		<a href="#pricing" class="text-gray-800">Pricing</a>
+		<a href="#blog" class="text-gray-800">Blog</a>
+		<button
+			class="rounded-xl border border-black bg-white px-4 py-2 text-black transition ease-in-out hover:bg-[#B9FF66] md:px-[35px] md:py-[20px]"
+		>
+			Request a Quote
+		</button>
+	</div>
+	<div class="md:hidden">
+		<button id="menu-button" class="text-gray-800">
+			<i class="fas fa-bars"></i>
+		</button>
+	</div>
+</div>
+
+<!-- Mobile Menu -->
+<div
+	id="mobile-menu"
+	class="flex hidden flex-col items-start gap-4 bg-white p-4 shadow-md md:hidden"
+>
+	<a href="#about-us" class="text-gray-800">About Us</a>
+	<a href="#services" class="text-gray-800">Services</a>
+	<a href="#use-cases" class="text-gray-800">Use Cases</a>
+	<a href="#pricing" class="text-gray-800">Pricing</a>
+	<a href="#blog" class="text-gray-800">Blog</a>
+	<button
+		class="rounded-xl border border-black bg-white px-4 py-2 text-black transition ease-in-out hover:bg-[#B9FF66]"
+	>
+		Request a Quote
+	</button>
+</div>
